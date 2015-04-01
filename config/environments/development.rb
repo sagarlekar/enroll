b46f92be8a6b7c32f7803a1a04dc720f5a26cebc
Ken Lu <ken@seeice.com>
2015-03-31 23:31:55 -0400

    receive msg from queue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,10 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
   config.acapi.publish_amqp_events = true
+  config.acapi.app_id = "enroll"
+  config.acapi.remote_broker_uri = "amqp://guest:guest@localhost:5672"
+  config.acapi.remote_event_queue = "acapi.events.local"
+  config.acapi.remote_request_exchange = ""
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
--- config/initializers/notifications.rb
+++ config/initializers/notifications.rb
@@ -2,16 +2,20 @@ require 'acapi/subscribers/base'
 require 'acapi/subscribers/enterprise_logger'
 require 'acapi/subscribers/logger'
 require 'acapi/publishers/logger'
+require "acapi/publishers/upstream_event_publisher"
 
 ActiveSupport::Notifications.subscribe "process_action.action_controller" do |name, start, finish, id, payload|
   Rails.logger.debug(["notification:", name, start, finish, id, payload].join(" "))
 end
 
-ActiveSupport::Notifications.subscribe "where" do |name, start, finish, id, payload|
+ActiveSupport::Notifications.subscribe "acapi.acapi.events.local" do |name, start, finish, id, payload|
+  byebug
   Rails.logger.debug(["notification:", name, start, finish, id, payload].join(" "))
 end 
 
 Acapi::Subscribers::Logger.register("acapi.logger")
 # Acapi::Subscribers::Logger.register("acapi.edi_event")
 
-Acapi::Publishers::Logger.listen_queue("acapi.logger", "re.logger")
+#Acapi::Publishers::Logger.listen_queue("acapi.logger", "re.logger")
+
+Acapi::Publishers::UpstreamEventPublisher.new.run
